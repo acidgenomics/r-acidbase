c25eeeb2e2e4b0b2a80df9d6782fe2d444d954d1
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-21 17:07:11 -0400

    Add basic capitalization functions

--- R/capitalize.R
+++ R/capitalize.R
@@ -1,26 +1,61 @@
 #' Capitalize
 #'
-#' Applies sentence case style capitalization.
+#' Apply simple capitlization rules.
 #'
-#' Only capitalizes the first letter in the string.
+#' For complex renaming, refer to syntactic package.
 #'
-#' @export
+#' @name capitalize
 #' @note Updated 2019-10-21.
 #'
 #' @inheritParams params
 #'
 #' @return Modified object, with names capitalized.
 #'
+#' @seealso
+#' - `toupper()`, `tolower()`.
+#' - stringr package.
+#' - `tools::toTitleCase()`. I generally avoid title case, since there's a poor
+#'   general consensus on which works to keep lower case.
+#'
 #' @examples
-#' capitalize(c("hello", "world", "foo bar"))
+#' x <- c("the quick brown fox", "using AIC for model selection")
+#' capitalize(x)
+#' sentenceCase(x)
+NULL
+
+
+
+#' @describeIn capitalize Capitalize the first letter of all words.
+#' @export
 capitalize <- function(x) {
+    vapply(
+        X = as.character(x),
+        FUN = function(x) {
+            x <- strsplit(x, " ")[[1L]]
+            paste(
+                toupper(substring(x, 1L, 1L)),
+                substring(x, 2L),
+                sep = "",
+                collapse = " "
+            )
+        },
+        FUN.VALUE = character(1L),
+        USE.NAMES = FALSE
+    )
+}
+
+
+
+#' @describeIn capitalize Only capitalize the first letter in the sentence.
+#' @export
+sentenceCase <- function(x) {
     vapply(
         X = as.character(x),
         FUN = function(x) {
             if (is.na(x)) return(x)
             first <- substring(x, first = 1L, last = 1L)
             first <- toupper(first)
-            tail <- substring(x, first = 2L)
+            tail <- tolower(substring(x, first = 2L))
             paste(first, tail, sep = "")
         },
         FUN.VALUE = character(1L),
--- tests/testthat/test-capitalize.R
+++ tests/testthat/test-capitalize.R
@@ -1,5 +1,8 @@
 context("capitalize")
 
-test_that("capitalize", {
-    expect_identical(capitalize("foo bar"), "Foo bar")
+test_that("Vectorized", {
+    expect_identical(
+        object = capitalize(c("foo bar", "aaa", NA_character_)),
+        expected = c("Foo bar", "Aaa", NA_character_)
+    )
 })
