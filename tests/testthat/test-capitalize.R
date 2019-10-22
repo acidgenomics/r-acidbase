b2804f26e89104d5a0adca1a4efd8e6650440a6d
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-21 21:36:52 -0400

    Remove unit test

--- tests/testthat/test-capitalize.R
+++
@@ -1,8 +0,0 @@
-context("capitalize")
-
-test_that("Vectorized", {
-    expect_identical(
-        object = capitalize(c("foo bar", "aaa", NA_character_)),
-        expected = c("Foo bar", "Aaa", NA_character_)
-    )
-})
