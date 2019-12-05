1833184719e9365717747bcd88b68de2c1014954
Michael Steinbaugh <mike@steinbaugh.com>
2019-12-05 09:35:05 -0500

    Improve code coverage when running on a very minimal R install

--- tests/testthat/test-methodDefinition.R
+++ tests/testthat/test-methodDefinition.R
@@ -1,6 +1,30 @@
 context("methodDefinition")
 
-test_that("as.data.frame", {
+test_that("methods 'show()'", {
+    skip_if_not_installed("methods")
+    x <- methodFunction(
+        f = "show",
+        signature = "ANY",
+        package = "methods"
+    )
+    expect_is(x, "function")
+    x <- methodFormals(
+        f = "show",
+        signature = "ANY",
+        package = "methods"
+    )
+    expect_is(x, "pairlist")
+})
+
+test_that("Expected failure", {
+    expect_error(
+        methodFunction("XXX", signature = "ANY", package = "methods"),
+        "Failed to locate"
+    )
+})
+
+test_that("S4Vectors 'as.data.frame()'", {
+    skip_if_not_installed("S4Vectors")
     ## Use `substitute()` to put a missing argument in pairlist.
     ## https://stackoverflow.com/questions/3892580
     formals <- pairlist(
@@ -9,7 +33,6 @@ test_that("as.data.frame", {
         optional = FALSE,
         "..." = substitute()
     )
-
     ## Function
     x <- methodFunction(
         f = "as.data.frame",
@@ -18,7 +41,6 @@ test_that("as.data.frame", {
     )
     expect_is(x, "function")
     expect_identical(formals(x), formals)
-
     ## Formals
     x <- methodFormals(
         f = "as.data.frame",
@@ -28,14 +50,9 @@ test_that("as.data.frame", {
     expect_identical(x, formals)
 })
 
-test_that("Expected failure", {
-    expect_error(
-        methodFunction("XXX", signature = "ANY", package = "S4Vectors"),
-        "Failed to locate"
-    )
-})
-
 test_that(".local handling", {
+    skip_if_not_installed("BiocGenerics")
+    skip_if_not_installed("S4Vectors")
     md <- getMethod(
         f = "aggregate",
         signature = "Vector",
