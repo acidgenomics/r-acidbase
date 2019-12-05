a4c10b12f5e11b6e559683ee200f93387da81f1b
Michael Steinbaugh <mike@steinbaugh.com>
2019-12-05 09:20:45 -0500

    Prepare release

--- R/fileDepth.R
+++ R/fileDepth.R
@@ -1,23 +1,27 @@
 #' File depth
 #'
-#' This function is parameterized, and works on multiple files and/or
-#' directories.
+#' @export
+#' @note This function intentionally doesn't check whether a file exists.
+#' @note Updated 2019-09-05.
 #'
-#' File path(s) do not have to exist on disk, similar to the approach employed
-#' by [`file.path()`][base::file.path].
+#' @param path `character`.
+#'   File path(s).
+#'   This function is vectorized and supports multiple files.
+#' @param fsep `character(1)`.
+#'   Platform-specific file path separator (e.g. "/" on Unix).
 #'
+#' @return `integer`.
+#' Unnamed vector denoting file path depth.
 #' Note that "/" will return 0 here (zero indexed).
 #'
-#' @export
-#' @note Updated 2019-09-05.
+#' @examples
 #' a <- tempdir()
 #' b <- tempfile()
 #' fileDepth(a, b)
-fileDepth <- function(..., fsep = .Platform[["file.sep"]]) {
-    x <- unlist(list(...))
-    x <- normalizePath(x, winslash = fsep, mustWork = FALSE)
+fileDepth <- function(path, fsep = .Platform[["file.sep"]]) {
+    path <- normalizePath(path, winslash = fsep, mustWork = FALSE)
     vapply(
-        X = x,
+        X = path,
         FUN = function(x) {
             x <- strsplit(x = x, split = fsep, fixed = TRUE)
             x <- unlist(x)
