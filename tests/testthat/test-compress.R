eca1d50a9271243d7f946167f6163288ce4c5e5e
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-19 11:29:49 -0500

    Draft update to unit test

---
+++ tests/testthat/test-compress.R
@@ -0,0 +1,32 @@
+context("compress")
+
+text <- c("hello","world")
+file <- "test.txt"
+writeLines(text = text, con = file)
+
+ext <- eval(formals(compress)[["ext"]])
+with_parameters_test_that(
+    desc_stub = toString(ext),
+    code = {
+        ## Compress the file.
+        zfile <- compress(
+            file = file,
+            ext = ext,
+            remove = TRUE,
+            overwrite = TRUE
+        )
+        expect_true(file.exists(zfile))
+        expect_false(file.exists(file))
+        ## Decompress the compressed file.
+        file <- decompress(file = zfile, remove = TRUE, overwrite = TRUE)
+        expect_true(file.exists(file))
+        expect_false(file.exists(zfile))
+        expect_identical(
+            object = readLines(con = file),
+            expected = text
+        )
+    },
+    ext = ext
+)
+
+unlink(file)
