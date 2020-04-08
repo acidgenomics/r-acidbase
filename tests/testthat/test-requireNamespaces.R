5ef0c3f67183cd72930ed7ebfa0d70d1403672e3
Michael Steinbaugh <mike@steinbaugh.com>
2020-04-08 08:12:42 -0400

    Add requireNamespaces to release

---
+++ tests/testthat/test-requireNamespaces.R
@@ -0,0 +1,15 @@
+context("requireNamespaces")
+
+test_that("Installed packages", {
+    packages <- c("base", "utils")
+    ok <- requireNamespaces(packages)
+    expect_true(ok)
+})
+
+test_that("Missing packages", {
+    packages <- c("AAA", "BBB")
+    expect_error(
+        object = requireNamespaces(packages),
+        regexp = toString(packages)
+    )
+})
