3bd7a20d740221c3bb53963ae91b129b294c725e
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-09 13:13:49 -0400

    Rethink parseArgs approach

--- R/parseArgs.R
+++ R/parseArgs.R
@@ -1,104 +1,150 @@
-## This function is covered inside Rscript calls defined in koopa package.
-## This is hard to unit test using testthat package.
-
-## nocov start
+## FIXME This needs to support `--days 2`.
+## FIXME This needs to error if the user passes in extra stuff.
+## FIXME Need to add Rscript unit tests for this.
+## FIXME Improve working example to include functional script.
+## FIXME NEED TO DEFINE REQUIREDFLAGS, REQUIREDARGS
 
 
 
 #' Parse command line argument flags
 #'
 #' @export
-#' @note Updated 2020-04-07.
+#' @note Updated 2020-08-09.
+#' @note Short flags, such as `-r`, are intentionally not supported.
 #'
-#' @param positional `logical(1)`.
-#'   Error if required positional arguments are missing.
-#' @param validArgs `character` or `NULL`.
-#'   Valid key-value pair argument names.
-#'   For example, `aaa` for `--aaa=AAA`.
-#' @param validFlags `character` or `NULL`.
-#'   Valid flag names.
+#' @param requiredFlags,optionalFlags `character` or `NULL`.
+#'   Valid long flag names.
 #'   For example, `aaa` for `--aaa`.
+#' @param requiredArgs,optionalArgs `character` or `NULL`.
+#'   Valid key-value pair argument names.
+#'   For example, `aaa` for `--aaa=AAA` or `--aaa AAA`.
+#'   Note that `--aaa AAA`-style arguments are not currently supported.
+#' @param positional `logical(1)`.
+#'   Error if required positional arguments (>= 1) are missing.
 #'
-#' @return `character`.
-#'   Arguments.
+#' @return `list`.
+#'   Named list containing arguments, organized by type:
+#'   - requiredFlags
+#'   - optionalFlags
+#'   - requiredArgs
+#'   - optionalArgs
+#'   - positionalArgs
 #'
 #' @seealso
-#' - argparse Python package
-#' - argparser R package
-#' - optparse R package
+#' - argparse Python package.
+#' - argparser R package.
+#' - optparse R package.
 #'
 #' @examples
-#' parseArgs()
-#'
-#' ## $ Rscript test.Rscript --a="A" --b
-#' ## > parseArgs(positional = FALSE, validArgs = "a", validFlags = "b")
+#' command <- system.file("scripts", "parse-args", package = "acidbase")
+#' args <- c(
+#'     ## Required flags:
+#'     "--aaa", "--bbb",
+#'     ## Optional flags:
+#'     "--ccc", "--ddd",
+#'     ## Required args:
+#'     "--eee=EEE", "--fff=FFF",
+#'     ## Optional args:
+#'     "--ggg=GGG", "--hhh=HHH",
+#'     ## Positional args:
+#'     "III", "JJJ"
+#' )
+#' readLines(command)
+#' shell(command = command, args = args)
 parseArgs <- function(
-    positional = FALSE,
-    validArgs = NULL,
-    validFlags = NULL
+    requiredFlags = NULL,
+    optionalFlags = NULL,
+    requiredArgs = NULL,
+    optionalArgs = NULL,
+    positionalArgs = FALSE
 ) {
-    if (isTRUE(positional)) {
-        posArgs <- positionalArgs()
-        if (!.hasLength(posArgs)) {
-            stop("Required positional arguments are missing.")
+    stopifnot(
+        !.hasLength(intersect(requiredFlags, optionalFlags)),
+        !.hasLength(intersect(requiredFlags, requiredArgs)),
+        !.hasLength(intersect(requiredFlags, optionalArgs)),
+        !.hasLength(intersect(optionalFlags, requiredArgs)),
+        !.hasLength(intersect(optionalFlags, optionalArgs)),
+        !.hasLength(intersect(requiredArgs, optionalArgs))
+    )
+    cmdArgs <- commandArgs(trailingOnly = TRUE)
+    out <- list(
+        requiredFlags = NULL,
+        optionalFlags = NULL,
+        requiredArgs = NULL,
+        optionalArgs = NULL,
+        positionalArgs = NULL
+    )
+    if (!is.null(requiredFlags) || !is.null(optionalFlags)) {
+        flagPattern <- "^--([^=[:space:]]+)$"
+        flags <- grep(pattern = flagPattern, x = cmdArgs, value = TRUE)
+        flagNames <- gsub(pattern = flagPattern, replacement = "\\1", x = flags)
+        if (!is.null(requiredFlags)) {
+            match <- match(x = requiredFlags, table = flagNames)
+            if (any(is.na(match))) {
+                fail <- flags[is.na(match)]
+                stop(sprintf(
+                    "Failed to match required flag: %s.",
+                    toString(fail)
+                ))
+            }
+            hits <- flags[match]
+            names(hits) <- flagNames[match]
+            out[["requiredFlags"]] <- hits
+            flags <- setdiff(flags, hits)
+            flagNames <- setdiff(flagNames, names(hits))
+            cmdArgs <- setdiff(cmdArgs, hits)
         }
-    }
-    args <- commandArgs(trailingOnly = TRUE)
-    keep <- grepl(pattern = "^--", x = args)
-    x <- args[keep]
-    ## Check for valid arguments.
-    if (!is.null(validArgs) || !is.null(validFlags)) {
-        ## Get the argument names (`--a="XXX"` to "a").
-        pattern <- "^--([-a-z0-9]+)=.+$"
-        keep <- grepl(pattern = pattern, x = x)
-        args <- x[keep]
-        argNames <- gsub(pattern = pattern, replacement = "\\1", x = args)
-        stopifnot(!any(duplicated(argNames)))
-        ## Get the flag names (`--a` to "a").
-        keep <- grepl(pattern = "^--[-a-z0-9]+$", x = x)
-        flags <- x[keep]
-        flagNames <- gsub(pattern = "^--", replacement = "", x = flags)
-        stopifnot(
-            !any(duplicated(flagNames)),
-            !.hasLength(intersect(argNames, flagNames)),
-            !.hasLength(intersect(argNames, validFlags)),
-            !.hasLength(intersect(flagNames, validArgs))
-        )
-    }
-    ## Check for valid arguments.
-    if (!is.null(validArgs)) {
-        if (!all(argNames %in% validArgs)) {
-            invalid <- setdiff(argNames, validArgs)
-            stop(sprintf(
-                fmt = "Invalid %s: '%s'.",
-                ngettext(
-                    n = length(invalid),
-                    msg1 = "arg",
-                    msg2 = "args"
-                ),
-                toString(invalid)
-            ))
+        if (!is.null(optionalFlags)) {
+            match <- match(x = optionalFlags, table = flagNames)
+            if (any(!is.na(match))) {
+                hits <- flags[!is.na(match)]
+                names(hits) <- flagNames[!is.na(match)]
+                out[["optionalFlags"]] <- hits
+                flags <- setdiff(flags, hits)
+                flagNames <- setdiff(flagNames, names(hits))
+                cmdArgs <- setdiff(cmdArgs, hits)
+            }
         }
+        stopifnot(!.hasLength(flags), !.hasLength(flagNames))
     }
-    ## Check for valid flags.
-    if (!is.null(validFlags)) {
-        if (!all(flagNames %in% validFlags)) {
-            invalid <- setdiff(flagNames, validFlags)
-            stop(sprintf(
-                fmt = "Invalid %s: '%s'.",
-                ngettext(
-                    n = length(invalid),
-                    msg1 = "flag",
-                    msg2 = "flags"
-                ),
-                toString(invalid)
-            ))
+    if (!is.null(requiredArgs) || !is.null(optionalArgs)) {
+        argPattern <- "^--([^=[:space:]]+)=([^[:space:]]+)$"
+        args <- grep(pattern = argPattern, x = cmdArgs, value = TRUE)
+        argNames <- gsub(pattern = argPattern, replacement = "\\1", x = args)
+        if (!is.null(requiredArgs)) {
+            match <- match(x = requiredArgs, table = argNames)
+            if (any(is.na(match))) {
+                fail <- args[is.na(match)]
+                stop(sprintf(
+                    "Failed to match required arg: %s.",
+                    toString(fail)
+                ))
+            }
+            hits <- args[match]
+            names(hits) <- argNames[match]
+            out[["requiredArgs"]] <- hits
+            args <- setdiff(args, hits)
+            argNames <- setdiff(argNames, names(hits))
+            cmdArgs <- setdiff(cmdArgs, hits)
         }
+        if (!is.null(optionalArgs)) {
+            match <- match(x = optionalArgs, table = argNames)
+            if (any(!is.na(match))) {
+                hits <- args[!is.na(match)]
+                names(hits) <- argNames[!is.na(match)]
+                out[["optionalArgs"]] <- hits
+                args <- setdiff(args, hits)
+                argNames <- setdiff(argNames, names(hits))
+                cmdArgs <- setdiff(cmdArgs, hits)
+            }
+        }
+        stopifnot(!.hasLength(args), !.hasLength(argNames))
     }
-    ## Return arguments.
-    x
+    if (!isTRUE(positionalArgs)) {
+        stopifnot(.hasLength(cmdArgs))
+        out[["positionalArgs"]] <- cmdArgs
+    } else {
+        stopifnot(!.hasLength(cmdArgs))
+    }
+    out
 }
-
-
-
-## nocov end
