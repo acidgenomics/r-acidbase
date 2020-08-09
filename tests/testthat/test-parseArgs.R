fe59a4e6e3fea8424fcbc2d47b4dc9b744103a69
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-09 13:20:05 -0400

    Add code coverage

--- tests/testthat/test-parseArgs.R
+++ tests/testthat/test-parseArgs.R
@@ -1,6 +1,29 @@
 context("parseArgs")
 
-## This function is well-covered inside koopa package.
+test_that("Working example", {
+    command <- system.file("scripts", "parse-args", package = "acidbase")
+    args <- c(
+        ## Required flags:
+        "--aaa", "--bbb",
+        ## Optional flags:
+        "--ccc", "--ddd",
+        ## Required args:
+        "--eee=EEE", "--fff=FFF",
+        ## Optional args:
+        "--ggg=GGG", "--hhh=HHH",
+        ## Positional args:
+        "III", "JJJ"
+    )
+    out <- shell(
+        command = command,
+        args = args,
+        stdout = TRUE
+    )
+    expect_match(
+        object = paste(out, collapse = "\n"),
+        regexp = "requiredFlags"
+    )
+})
 
 test_that("No arguments", {
     expect_identical(
