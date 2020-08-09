9de5d1da35688e64650c5c99b48163adfbd7fafa
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-09 15:36:57 -0400

    Fix handling

--- R/parseArgs.R
+++ R/parseArgs.R
@@ -94,7 +94,7 @@ parseArgs <- function(
             out[["requiredArgs"]] <- args[requiredArgs]
             args <- args[!names(args) %in% requiredArgs]
         }
-        if (!is.null(optionalArgs)) {
+        if (!is.null(optionalArgs) && .hasLength(args)) {
             match <- match(x = names(args), table = optionalArgs)
             if (any(is.na(match))) {
                 fail <- names(args)[is.na(match)]
