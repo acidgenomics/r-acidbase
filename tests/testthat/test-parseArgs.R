1a0a3187ef01074ef357026fe4b0b9434e540083
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-09 13:43:45 -0400

    Rework unit tests

--- tests/testthat/test-parseArgs.R
+++ tests/testthat/test-parseArgs.R
@@ -25,12 +25,82 @@ test_that("Working example", {
     )
 })
 
-test_that("Required flags mismatch", {
-    command <- file.path("parseArgs", "required-flags-error")
+test_that("Missing required flags", {
+    command <- file.path("parseArgs", "required-flags")
     args <- c("--aaa", "--bbb")
-    shell(
+    out <- shell(
         command = command,
-        args = args
+        args = args,
+        stderr = TRUE
     )
+    expect_match(
+        object = paste(out, collapse = "\n"),
+        regexp = "Failed to match required flag"
+    )
+})
 
+test_that("Missing required args", {
+    command <- file.path("parseArgs", "required-args")
+    args <- c("--aaa=AAA", "--bbb=BBB")
+    out <- shell(
+        command = command,
+        args = args,
+        stderr = TRUE
+    )
+    expect_match(
+        object = paste(out, collapse = "\n"),
+        regexp = "Failed to match required arg"
+    )
+})
+
+test_that("Unset args or flags as positional args", {
+    command <- file.path("parseArgs", "positional-args")
+    args <- c("--aaa", "--bbb=BBB")
+    out <- shell(
+        command = command,
+        args = args,
+        stderr = TRUE
+    )
+    expect_match(
+        object = paste(out, collapse = "\n"),
+        regexp = "positionalArgs"
+    )
+})
+
+test_that("Missing positional args", {
+    command <- file.path("parseArgs", "positional-args")
+    args <- ""
+    out <- shell(
+        command = command,
+        args = args,
+        stderr = TRUE
+    )
+    expect_match(
+        object = paste(out, collapse = "\n"),
+        regexp = "positionalArgs"
+    )
+})
+
+test_that("No positional args allowed", {
+    command <- file.path("parseArgs", "no-positional-args")
+    args <- ""
+    out <- shell(
+        command = command,
+        args = args,
+        stdout = TRUE
+    )
+    expect_match(
+        object = paste(out, collapse = "\n"),
+        regexp = "positionalArgs"
+    )
+    args <- c("aaa", "bbb")
+    out <- shell(
+        command = command,
+        args = args,
+        stderr = TRUE
+    )
+    expect_match(
+        object = paste(out, collapse = "\n"),
+        regexp = "hasLength"
+    )
 })
