bab40e6526fe1bc9fb2a358cd7ebcb2dca9405fc
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-07 07:32:53 -0400

    Add headtail and showSlotInfo

--- R/imports.R
+++ R/imports.R
@@ -1,3 +1,4 @@
-#' @importFrom methods is isGeneric selectMethod slot
+#' @importFrom methods is isGeneric selectMethod setMethod setOldClass signature
+#'   slot
 #' @importFrom utils capture.output head unzip zip
 NULL
