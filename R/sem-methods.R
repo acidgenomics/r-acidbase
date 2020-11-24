188e752019dc30f4bc557f2a2fff4e3bc6050592
Michael Steinbaugh <mike@steinbaugh.com>
2020-11-24 12:31:02 -0500

    Add sem method

---
+++ R/sem-methods.R
@@ -0,0 +1,29 @@
+#' @name sem
+#' @inherit AcidGenerics::sem
+#' @note Updated 2020-11-24.
+#'
+#' @inheritParams AcidRoxygen::params
+#' @param ... Additional arguments.
+#'
+#' @details
+#' Alternatively, can use: `sd(x) / sqrt(length(x))`.
+#'
+#' @seealso
+#' - https://stackoverflow.com/questions/2676554/
+NULL
+
+
+
+`sem,numeric` <- function(x) {
+    sqrt(var(x) / length(x))
+}
+
+
+
+#' @rdname sem
+#' @export
+setMethod(
+    f = "sem",
+    signature = signature("numeric"),
+    definition = `sem,numeric`
+)
