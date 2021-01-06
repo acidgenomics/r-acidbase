1dc66cc179c656a86ca9e6502e825a63c732ca1f
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-06 11:15:02 -0500

    Reexport some functions from goalie, previously defined here

---
+++ R/reexports.R
@@ -0,0 +1,33 @@
+## FIXME SEE WHICH OF THESE ARE ONLY USED IN GOALIE AND REMOVE.
+
+#' @importFrom goalie bapply
+#' @export
+goalie::bapply
+
+#' @importFrom goalie compressExtPattern
+#' @export
+goalie::compressExtPattern
+
+#' @importFrom goalie extPattern
+#' @export
+goalie::extPattern
+
+#' @importFrom goalie getNameInParent
+#' @export
+goalie::getNameInParent
+
+#' @importFrom goalie printString
+#' @export
+goalie::printString
+
+#' @importFrom goalie safeDeparse
+#' @export
+goalie::safeDeparse
+
+#' @importFrom goalie shorten
+#' @export
+goalie::shorten
+
+#' @importFrom goalie toNames
+#' @export
+goalie::toNames
