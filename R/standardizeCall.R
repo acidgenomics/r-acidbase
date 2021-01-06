4590a0ef364566bd9816c9a5dce28b623f8c304e
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-06 12:16:24 -0500

    Prepare release

--- R/standardizeCall.R
+++ R/standardizeCall.R
@@ -144,7 +144,7 @@ standardizeCall <- function(
 ## Updated 2019-10-21.
 .isLocalCall <- function(x) {
     ok <- is.call(x)
-    if (!isTRUE(ok)) return(FALSE)
+    if (!isTRUE(ok)) return(FALSE)  # nocov
     ok <- identical(x[[1L]], as.symbol(".local"))
     if (!isTRUE(ok)) return(FALSE)
     TRUE
