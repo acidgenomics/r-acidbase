eb6756846a9d5a2619d3dc2cecc37c7f59234136
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-28 14:52:32 -0500

    Add code coverage for `classContains`

--- R/imports.R
+++ R/imports.R
@@ -4,8 +4,8 @@
 #' @importFrom goalie assert bapply compressExtPattern extPattern hasLength
 #'   hasNames hasNoDuplicates isADir isAFile isCharacter isFlag isInt isScalar
 #'   isString
-#' @importFrom methods is isGeneric selectMethod setMethod setOldClass signature
-#'   slot
+#' @importFrom methods getClassDef is isGeneric selectMethod setMethod
+#'   setOldClass signature slot
 #' @importFrom stats var
 #' @importFrom utils capture.output download.file head tail unzip zip
 NULL
