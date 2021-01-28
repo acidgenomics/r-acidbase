579bfc51d42d633c4dea40f3f20a491c74135b37
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-28 14:50:07 -0500

    Add `classContains` function

---
+++ R/classContains.R
@@ -0,0 +1,36 @@
+#' Get the class contained in a specificed class definition
+#'
+#' Determine which class is contained (extended) in the requested class
+#' definition.
+#'
+#' @export
+#' @note Updated 2021-01-28.
+#'
+#' @param class `character(1)`.
+#'   Class name.
+#' @param package `character(1)`.
+#'   Package name.
+#'
+#' @return `character(1)`.
+#'   Name of extended class.
+#'
+#' @seealso
+#' - [methods::getClass()].
+#' - help("classRepresentation-class", "methods")
+classContains <- function(class, package) {
+    assert(
+        isString(class),
+        isString(package)
+    )
+    args <- list(
+        "Class" = class,
+        "package" = package,
+        "inherits" = FALSE
+    )
+    x <- do.call(what = getClassDef, args = args)
+    assert(is(x, "classRepresentation"))
+    x <- attributes(x)
+    x <- names(x[["contains"]])[[1L]]
+    assert(isString(x))
+    x
+}
