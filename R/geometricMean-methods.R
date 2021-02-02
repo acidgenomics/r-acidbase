aa3ffedc7774581b146a48ca2b08d6a19c9ae03a
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-02 13:49:34 -0500

    Improve method definition

---
+++ R/geometricMean-methods.R
@@ -0,0 +1,123 @@
+## FIXME NEED TO RENAME PRIMARY ARGUMENT TO X HERE, FOR CONSISTENCY.
+## FIXME NEED AsIs method here.
+
+
+
+#' @name geometricMean
+#' @inherit AcidGenerics::geometricMean
+#'
+#' @note This function should be fully zero- and `NA`-tolerant. This calculation
+#'   is not particularly useful if there are elements that are <= 0 and will
+#'   return `NaN`.
+#' @note Updated 2021-02-02.
+#'
+#' @inheritParams AcidRoxygen::params
+#' @inheritParams base::apply
+#' @param ... Additional arguments.
+#'
+#' @seealso
+#' - [Paul McMurdie's code](https://stackoverflow.com/a/25555105).
+#' - `psych::geometric.mean()`.
+#'
+#' @return `numeric`.
+#'
+#' @examples
+#' ## numeric ====
+#' vec1 <- seq(1L, 5L, 1L)
+#' print(vec1)
+#' geometricMean(vec1)
+#'
+#' vec2 <- vec1 ^ 2L
+#' print(vec2)
+#' geometricMean(vec2)
+#'
+#' ## matrix ====
+#' matrix <- matrix(
+#'     data = c(vec1, vec2),
+#'     ncol = 2L,
+#'     byrow = FALSE
+#' )
+#' print(matrix)
+#' geometricMean(matrix)
+NULL
+
+
+
+## Necessary for dplyr mutate calls.
+## Updated 2021-02-02.
+`geometricMean,AsIs` <-  # nolint
+    function(x) {
+        geometricMean(as.numeric(x))
+    }
+
+
+
+#' @rdname geometricMean
+#' @export
+setMethod(
+    f = "geometricMean",
+    signature = signature("AsIs"),
+    definition = `geometricMean,AsIs`
+)
+
+
+
+## Updated 2020-02-02.
+`geometricMean,numeric` <-  # nolint
+    function(
+        x,
+        removeNA = TRUE,
+        zeroPropagate = FALSE
+    ) {
+        assert(
+            isFlag(removeNA),
+            isFlag(zeroPropagate)
+        )
+        ## Check for any negative numbers and return `NaN`.
+        if (any(x < 0L, na.rm = TRUE)) {
+            return(NaN)
+        }
+        if (isTRUE(zeroPropagate)) {
+            if (any(x == 0L, na.rm = TRUE)) {
+                return(0L)
+            }
+            exp(mean(log(x), na.rm = removeNA))
+        } else {
+            exp(
+                sum(log(x[x > 0L]), na.rm = removeNA) /
+                    length(x)
+            )
+        }
+    }
+
+
+
+#' @rdname geometricMean
+#' @export
+setMethod(
+    f = "geometricMean",
+    signature = signature("numeric"),
+    definition = `geometricMean,numeric`
+)
+
+
+
+## Updated 2020-02-02.
+`geometricMean,matrix` <-  # nolint
+    function(x, MARGIN = 2L) {  # nolint
+        apply(
+            X = x,
+            MARGIN = MARGIN,
+            FUN = geometricMean
+        )
+    }
+
+
+
+#' @rdname geometricMean
+#' @export
+setMethod(
+    f = "geometricMean",
+    signature = signature("matrix"),
+    definition = `geometricMean,matrix`
+)
