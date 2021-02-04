a7da8b8f4abefdd85435b9e24fa7037f9bdc0618
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-04 09:22:59 -0500

    Simplify documentation

--- R/package.R
+++ R/package.R
@@ -4,3 +4,31 @@
 #'
 #' @keywords internal
 "_PACKAGE"
+
+
+
+#' @importClassesFrom AcidGenerics Annotated AsIs
+#'
+#' @importFrom AcidGenerics metadata
+#' @importFrom AcidCLI alert alertWarning
+#' @importFrom goalie assert bapply compressExtPattern extPattern hasLength
+#'   hasNames hasNoDuplicates isADir isAFile isCharacter isFlag isInt isPositive
+#'   isScalar isString
+#' @importFrom methods .hasSlot getClassDef is isGeneric selectMethod setMethod
+#'   setOldClass signature slot slotNames
+#' @importFrom stats var
+#' @importFrom utils capture.output download.file head tail unzip zip
+NULL
+
+
+
+#' Shared parameter documentation
+#'
+#' @name params
+#'
+#' @param width `integer(1)`.
+#'   Width.
+#' @param x Object.
+#'
+#' @return No value.
+NULL
