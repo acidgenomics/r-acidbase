8ebe3f60ca3407bb2176485807b4a6c81458880f
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-11 15:59:49 -0500

    Don't reexport functions from methods

--- R/reexports.R
+++ R/reexports.R
@@ -2,10 +2,6 @@
 #' @export
 grid::arrow
 
-#' @importFrom methods as
-#' @export
-methods::as
-
 #' @importFrom stats as.formula
 #' @export
 stats::as.formula
@@ -42,10 +38,6 @@ stats::ecdf
 #' @export
 goalie::extPattern
 
-#' @importFrom methods formalArgs
-#' @export
-methods::formalArgs
-
 #' @importFrom goalie getNameInParent
 #' @export
 goalie::getNameInParent
@@ -58,10 +50,6 @@ utils::getS3method
 #' @export
 stats::hclust
 
-#' @importFrom methods is
-#' @export
-methods::is
-
 #' @importFrom AcidGenerics metadata
 #' @export
 AcidGenerics::metadata
@@ -73,10 +61,6 @@ AcidGenerics::`metadata<-`
 #' @importFrom stats model.matrix
 #' @export
 stats::model.matrix
-
-#' @importFrom methods new
-#' @export
-methods::new
 
 #' @importFrom utils packageName
 #' @export
@@ -114,38 +98,6 @@ grDevices::rgb
 #' @export
 utils::sessionInfo
 
-#' @importFrom methods setClass
-#' @export
-methods::setClass
-
-#' @importFrom methods setClassUnion
-#' @export
-methods::setClassUnion
-
-#' @importFrom methods setGeneric
-#' @export
-methods::setGeneric
-
-#' @importFrom methods setMethod
-#' @export
-methods::setMethod
-
-#' @importFrom methods setReplaceMethod
-#' @export
-methods::setReplaceMethod
-
-#' @importFrom methods setValidity
-#' @export
-methods::setValidity
-
-#' @importFrom methods signature
-#' @export
-methods::signature
-
 #' @importFrom grid unit
 #' @export
 grid::unit
-
-#' @importFrom methods validObject
-#' @export
-methods::validObject
