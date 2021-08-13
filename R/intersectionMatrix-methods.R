e8af7a2dd858a0592200be196e908f7d42123a81
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-13 10:39:55 -0400

    Draft updates to improving intersectionMatrix code

--- R/intersectionMatrix-methods.R
+++ R/intersectionMatrix-methods.R
@@ -20,8 +20,29 @@ NULL
 ## Updated 2020-08-18.
 `intersectionMatrix,list` <-  # nolint
     function(object) {
-        assert(length(object) > 1L)
-        elements <- sort(unique(unlist(object)))
+        assert(
+            hasLength(object),
+            allAreAtomic(object),
+            all(bapply(
+                X = object,
+                FUN = isAny,
+                classes = c("character", "numeric")
+            )),
+            isString(unique(vapply(
+                X = object,
+                FUN = simpleClass,
+                FUN.VALUE = character(1L)
+            ))),
+            msg = paste(
+                "Invalid input.",
+                "Must contain list of all 'character' or 'numeric' values."
+            )
+        )
+        elements <- sort(unique(unlist(
+            x = object,
+            recursive = FALSE,
+            use.names = FALSE
+        )))
         mat <- vapply(
             X = object,
             FUN = function(x, elements) {
@@ -31,9 +52,32 @@ NULL
             FUN.VALUE = logical(length(elements)),
             USE.NAMES = TRUE
         )
-        rownames(mat) <- elements
+        rownames(mat) <- as.character(elements)
         mat
     }
+
+
+
+## FIXME Should use array definition here instead?
+
+
+## Updated 2021-08-13.
+`intersectionMatrix,matrix` <-  # nolint
+    function(object) {
+        stop("FIXME Need to support this.")
+    }
+
+
+
+## Updated 2021-08-13.
+`intersectionMatrix,data.frame` <-  # nolint
+    function(object) {
+        stop("FIXME Need to ensure that all columns are same class")
+    }
+
+## FIXME Harden against matrix
+## FIXME Harden against data.frame
+## FIXME Add DataFrame and Matrix methods.
 
 
 
