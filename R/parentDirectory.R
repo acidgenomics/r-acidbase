f7db0b70c51fcb58dbaca90999854add141c4849
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-03 11:59:14 -0400

    Improve internal support for handling URLs

--- R/parentDirectory.R
+++ R/parentDirectory.R
@@ -12,38 +12,89 @@
 #' @param n `logical(1)`.
 #'   Number of levels to recursive.
 #'   Defaults to the immediate parent, similar to `dirname`.
-#' @param fsep `character(1)`.
-#'   Path separator to use (e.g. `"/"`).
 #'
 #' @return `character`.
 #'
 #' @examples
-#' parentDir(path = getwd())
+#' dir <- getwd()
+#' url <- pasteURL(
+#'     "r.acidgenomics.com",
+#'     "packages",
+#'     "acidbase",
+#'     "reference",
+#'     "parentDirectory.html",
+#'     protocol = "https"
+#' )
+#' path <- c(dir, url)
+#' parentDir(path = path)
 parentDirectory <- function(
     path,
-    n = 1L,
-    fsep
+    n = 1L
 ) {
     assert(
         isCharacter(path),
         isInt(n),
         isPositive(n),
-        isTRUE(is.finite(n)),
-        isString(fsep)
+        isTRUE(is.finite(n))
     )
     names <- names(path)
-    x <- strsplit(x = path, split = fsep, fixed = TRUE)
-    x <- vapply(
-        X = x,
-        n = n,
-        fsep = fsep,
-        FUN = function(x, n, fsep) {
-            x <- x[1L:(length(x) - n)]
-            x <- do.call(
-                what = file.path,
-                args = append(x = x, values = list("fsep" = fsep))
+    fileParent <- function(path, n) {
+        x <- realpath(path)
+        x <- dirname(x)
+        n <- n - 1L
+        if (isPositive(n)) {
+            x <- vapply(
+                X = x,
+                n = n,
+                FUN = function(x, n) {
+                    do.call(
+                        what = file.path,
+                        args = as.list(
+                            append(
+                                x = x,
+                                values = rep("..", times = n)
+                            )
+                        )
+                    )
+                },
+                FUN.VALUE = character(1L),
+                USE.NAMES = FALSE
             )
-            x
+            x <- realpath(x)
+        }
+        x
+    }
+    urlParent <- function(path, n) {
+        x <- path
+        assert(isTRUE(grepl(pattern = "://", x = x, fixed = TRUE)))
+        x <- strsplit(x = x, split = "/", fixed = TRUE)[[1L]]
+        protocol <- sub(pattern = ":$", replacement = "", x = x[[1L]])
+        x <- x[3L:length(x)]
+        assert(
+            n <= length(x),
+            msg = sprintf(
+                "Invalid parent (%s = %d) for '%s'.",
+                "n", n, path
+            )
+        )
+        x <- x[1L:(length(x) - n)]
+        do.call(
+            what = pasteURL,
+            args = append(
+                x = x,
+                values = list("protocol" = protocol)
+            )
+        )
+    }
+    x <- vapply(
+        X = path,
+        n = n,
+        FUN = function(path, n) {
+            ifelse(
+                test = isAURL(path),
+                yes = urlParent(path = path, n = n),
+                no = fileParent(path = path, n = n)
+            )
         },
         FUN.VALUE = character(1L),
         USE.NAMES = FALSE
@@ -51,8 +102,6 @@ parentDirectory <- function(
     names(x) <- names
     x
 }
-
-formals(parentDirectory)[["fsep"]] <- formals(file.path)[["fsep"]]
 
 
 
