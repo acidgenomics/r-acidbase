d987365935b990385f4d6bd3176520e3bb321ec0
Michael Steinbaugh <mike@steinbaugh.com>
2022-02-07 13:00:59 -0500

    Reorganize documentation

---
+++ R/imports.R
@@ -0,0 +1,15 @@
+#' @importClassesFrom BiocGenerics AsIs
+#' @importClassesFrom S4Vectors Annotated
+#'
+#' @importFrom AcidCLI abort alert alertWarning toInlineString
+#' @importFrom S4Vectors metadata metadata<-
+#' @importFrom goalie allAreAtomic allAreDirs allHaveAccess assert bapply
+#'   compressExtPattern extPattern hasLength hasNames hasNoDuplicates isADir
+#'   isAFile isAURL isAny isCharacter isFlag isInt isPositive isScalar isString
+#'   isSubset isSystemCommand
+#' @importFrom methods .hasSlot getClassDef is isGeneric selectMethod
+#'   setClassUnion setMethod setOldClass signature slot slotNames
+#' @importFrom processx run
+#' @importFrom stats var
+#' @importFrom utils capture.output download.file head unzip tail zip
+NULL
---
+++ R/params.R
@@ -0,0 +1,10 @@
+#' Shared parameter documentation
+#'
+#' @name params
+#'
+#' @param width `integer(1)`.
+#'   Width.
+#' @param x Object.
+#'
+#' @return No value.
+NULL
