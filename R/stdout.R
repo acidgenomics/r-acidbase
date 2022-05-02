4b09df05b38b547e51737f89775c1e8133cdfb32
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-02 10:39:36 -0400

    Export stdout utility

---
+++ R/stdout.R
@@ -0,0 +1,26 @@
+#' Split stdout string into a character vector
+#'
+#' @export
+#' @note Updated 2022-05-02.
+#'
+#' @return `character`.
+#'
+#' @seealso [shell()].
+#'
+#' @examples
+#' x <- shell(
+#'     command = "printf",
+#'     args = c("%s\n", "aaa", "bbb"),
+#'     print = FALSE
+#' )
+#' print(x)
+#' y <- stdout(x)
+#' print(y)
+stdout <- function(x) {
+    assert(
+        is.list(x),
+        isSubset("stdout", names(x))
+    )
+    out <- strsplit(x = x[["stdout"]], split = "\n", fixed = TRUE)[[1L]]
+    out
+}
