e7edec2e2dce84d6b1dd28dcd0f92a6e0df553d2
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-02 09:02:15 -0400

    Improve code coverage

--- tests/testthat/test-unlink2.R
+++ tests/testthat/test-unlink2.R
@@ -1,6 +1,7 @@
 test_that("unlink2", {
     object <- tempdir2()
     expect_true(dir.exists(object))
-    unlink2(object)
+    out <- unlink2(object)
     expect_false(dir.exists(object))
+    expect_identical(out, 0L)
 })
