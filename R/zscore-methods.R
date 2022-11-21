1acac809786eeb49e8d3448f01e213f673af7b9a
Michael Steinbaugh <mike@steinbaugh.com>
2022-11-21 10:23:15 -0500

    Initial zscore commit

---
+++ R/zscore-methods.R
@@ -0,0 +1,69 @@
+#' @name zscore
+#' @inherit AcidGenerics::zscore
+#'
+#' @note Updated 2022-11-17.
+#'
+#' @inheritParams AcidRoxygen::params
+#' @param ... Additional arguments.
+#'
+#' @seealso
+#' - `scale`.
+#'
+#' @examples
+#' data(matrix, package = "AcidTest")
+#'
+#' ## numeric ====
+#' object <- matrix[1L, ]
+#' zscore(object)
+#'
+#' ## matrix ====
+#' object <- matrix
+#' zscore(matrix, MARGIN = 1L)
+NULL
+
+
+
+## Alternative approach:
+## > scale(object, center = TRUE, scale = TRUE)
+## Updated 2022-11-17.
+`zscore,numeric` <- # nolint
+    function(object) {
+        assert(isFALSE(anyNA(object)))
+        (object - mean(object)) / sd(object)
+    }
+
+
+
+## Alternative approach:
+## > t(scale(t(object), center = TRUE, scale = TRUE))
+## Updated 2022-11-17.
+`zscore,matrix` <- # nolint
+    function(object, MARGIN = 1L) {
+        assert(isFALSE(anyNA(object)))
+        out <- apply(
+            X = object,
+            MARGIN = MARGIN,
+            FUN = zscore,
+            simplify = TRUE
+        )
+        out <- t(out)
+        out
+    }
+
+
+
+#' @rdname zscore
+#' @export
+setMethod(
+   f = "zscore",
+    signature = signature(object = "numeric"),
+    definition = `zscore,numeric`
+)
+
+#' @rdname zscore
+#' @export
+setMethod(
+    f = "zscore",
+    signature = signature(object = "matrix"),
+    definition = `zscore,matrix`
+)
