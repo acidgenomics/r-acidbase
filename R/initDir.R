a6bba4d09cf6603c19da9c9b52ece5a34136b0ae
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-16 14:23:10 -0400

    Ensure initDir is vectorized

--- R/initDir.R
+++ R/initDir.R
@@ -1,7 +1,3 @@
-# FIXME Add parameterization support here.
-
-
-
 #' Initialize directory
 #'
 #' Checks and creates a directory recursively automatically.
@@ -10,25 +6,33 @@
 #' directory.
 #'
 #' @export
-#' @note Updated 2022-06-02.
+#' @note Updated 2023-05-16.
 #'
-#' @param dir `character(1)`.
-#' Directory path.
+#' @param dir `character`.
+#' Directory path(s).
 #'
-#' @return `character(1)`.
-#' Directory path.
+#' @return `character`.
+#' Directory path(s).
 #'
 #' @examples
-#' dir <- file.path(tempdir(), "testdir")
+#' dir <- file.path(tempdir2(), c("aaa", "bbb"))
 #' dir.exists(dir)
 #' dir <- initDir(dir)
 #' dir.exists(dir)
 #' print(dir)
 #' unlink2(dir)
 initDir <- function(dir) {
-    assert(isString(dir))
-    if (!isADir(dir)) {
-        dir.create(dir, recursive = TRUE, showWarnings = FALSE)
-    }
+    assert(isCharacter(dir))
+    invisible(bapply(
+        X = dir,
+        FUN = function(dir) {
+            if (isADir(dir)) {
+                return(FALSE)
+            }
+            dir.create(path = dir, recursive = TRUE, showWarnings = FALSE)
+        },
+        USE.NAMES = FALSE
+    ))
+    assert(allAreDirs(dir))
     invisible(realpath(dir))
 }
