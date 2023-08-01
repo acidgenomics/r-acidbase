8dfae65a55ff2f90cf1187ea8c71d56c28c6b57f
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-01 08:32:00 -0400

    Simplify our tempdir2 function to only use a random string

--- R/tempdir2.R
+++ R/tempdir2.R
@@ -1,27 +1,21 @@
-## FIXME Simplify to only include a random string here?
-## FIXME Usage of sys.info user may be problematic on some systems...
-
-## FIXME Export a randomString function that serves as the generator here.
-
-
 #' Unique temporary directory
 #'
 #' @export
-#' @note Updated 2022-06-02.
+#' @note Updated 2023-08-01.
 #'
 #' @return `character(1L)`.
 #' Temporary directory path.
+#'
+#' @seealso
+#' - `randomString()`.
 #'
 #' @examples
 #' x <- tempdir2()
 #' print(x)
 tempdir2 <- function() {
-    user <- Sys.info()[["user"]]
-    timestamp <- as.numeric(Sys.time())
-    rand <- paste0(letters[sample(x = 26L, size = 10L)], collapse = "")
-    bn <- paste(user, timestamp, rand, sep = "-")
-    path <- file.path(tempdir(), bn)
-    assert(!dir.exists(path))
-    out <- initDir(path)
+    rand <- randomString(n = 10L)
+    dir <- file.path(tempdir(), rand)
+    assert(!dir.exists(dir))
+    out <- initDir(dir)
     out
 }
