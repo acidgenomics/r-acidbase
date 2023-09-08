fa861374272096659e4f1a00fada26b111e63e4f
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-08 15:13:42 -0400

    Add code coverage

---
+++ tests/testthat/test-keepOnlyAtomicCols.R
@@ -0,0 +1,16 @@
+test_that("keepOnlyAtomicCols", {
+    object <- S4Vectors::DataFrame(
+        "a" = I(list(
+            c(1L, 2L),
+            c(3L, 4L),
+            c(5L, 6L)
+        )),
+        "b" = c(TRUE, TRUE, FALSE),
+        "c" = c("a", "b", "c"),
+        row.names = c("x", "y", "z")
+    )
+    expect_identical(
+        object = keepOnlyAtomicCols(object),
+        expected = object[, c("b", "c")]
+    )
+})
