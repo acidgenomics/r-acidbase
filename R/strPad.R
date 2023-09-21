505682e0f3267b7e2803317003cb438a9d2cce27
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-21 09:07:21 -0400

    Improve strMatch consistency with other variants

---
+++ R/strPad.R
@@ -0,0 +1,15 @@
+## FIXME Add support for this.
+
+#' Pad a string to minimum width
+#'
+#' @export
+#' @note Updated 2023-09-21.
+#'
+#' @param x `character`.
+#'
+#' @seealso
+#' - [stringr::str_pad()].
+#' - [sprintf()].
+#' - https://stringr.tidyverse.org/articles/from-base.html
+strPad <- function(x, width, side, pad) {
+}
