b4f009c46de5aad8e7cb5f818b5db38e41723d3f
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-21 06:50:44 -0400

    Include return

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -3,6 +3,8 @@
 #' @export
 #' @note Updated 2021-02-04.
 #'
+#' @return `missingOrNULL`.
+#'
 #' @examples
 #' showClass("missingOrNULL")
 setClassUnion(name = "missingOrNULL", members = c("missing", "NULL"))
