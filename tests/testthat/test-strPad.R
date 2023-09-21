c3407e1532415d1288ec6f695c9710db08ac91cb
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-21 10:44:52 -0400

    Add coverage

---
+++ tests/testthat/test-strPad.R
@@ -0,0 +1,20 @@
+context("strPad", {
+    x <- c("1", "10", "100")
+    width <- max(nchar(x))
+    expect_identical(
+        object = strPad(x = x, width = width, side = "left", pad = "0"),
+        expected = c("001", "010", "100")
+    )
+    expect_identical(
+        object = strPad(x = x, width = width, side = "right", pad = "0"),
+        expected = c("100", "100", "100")
+    )
+    expect_identical(
+        object = strPad(x = x, width = width, side = "left", pad = " "),
+        expected = c("  1", " 10", "100")
+    )
+    expect_identical(
+        object = strPad(x = x, width = width, side = "right", pad = " "),
+        expected = c("1  ", "10 ", "100")
+    )
+})
