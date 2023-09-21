1a58a2b69d0a6a8a04f218b652ac4a54dee90840
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-21 11:06:31 -0400

    Use isInstalled

--- R/internal-cli.R
+++ R/internal-cli.R
@@ -1,29 +1,33 @@
+## Updated 2023-09-21.
 .abort <- function(x) {
-    if (isTRUE(requireNamespace("AcidCLI", quietly = TRUE))) {
+    if (isInstalled("AcidCLI")) {
         AcidCLI::abort(x, call = FALSE)
     } else {
         stop(x, call. = FALSE)
     }
 }
 
+## Updated 2023-09-21.
 .alert <- function(x) {
-    if (isTRUE(requireNamespace("AcidCLI", quietly = TRUE))) {
+    if (isInstalled("AcidCLI")) {
         AcidCLI::alert(x)
     } else {
         message(x)
     }
 }
 
+## Updated 2023-09-21.
 .alertWarning <- function(x) {
-    if (isTRUE(requireNamespace("AcidCLI", quietly = TRUE))) {
+    if (isInstalled("AcidCLI")) {
         AcidCLI::alertWarning(x)
     } else {
         message(x)
     }
 }
 
+## Updated 2023-09-21.
 .toInlineString <- function(x, ...) {
-    if (isTRUE(requireNamespace("AcidCLI", quietly = TRUE))) {
+    if (isInstalled("AcidCLI")) {
         AcidCLI::toInlineString(x, ...)
     } else {
         toString(x)
