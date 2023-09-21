d24ce5d2117caf952656dc37b5628958e5f4f744
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-21 06:57:20 -0400

    Update coverage

--- tests/testthat/test-headtail.R
+++ tests/testthat/test-headtail.R
@@ -1,32 +1,31 @@
 test_that("atomic", {
-    expect_output(
+    expect_identical(
         object = headtail(c("A", "B")),
-        regexp = "A B"
+        expected = "A B"
     )
-    expect_output(
+    expect_identical(
         object = headtail(LETTERS),
-        regexp = "A B ... Y Z"
+        expected = "A B ... Y Z"
     )
 })
 
-matrix <- matrix(
-    data = sample(x = c(0L, 1L), size = 10000L, replace = TRUE),
-    nrow = 100L,
-    ncol = 100L,
-    byrow = FALSE,
-    dimnames = list(
-        paste0("gene", seq(from = 1L, to = 100L, by = 1L)),
-        paste0("sample", seq(from = 1L, to = 100L, by = 1L))
-    )
-)
-
 test_that("matrix", {
-    expect_output(
-        object = headtail(matrix),
+    mat <- matrix(
+        data = sample(x = c(0L, 1L), size = 10000L, replace = TRUE),
+        nrow = 100L,
+        ncol = 100L,
+        byrow = FALSE,
+        dimnames = list(
+            paste0("gene", seq(from = 1L, to = 100L, by = 1L)),
+            paste0("sample", seq(from = 1L, to = 100L, by = 1L))
+        )
+    )
+    expect_match(
+        object = headtail(mat)[[2L]],
         regexp = "gene1"
     )
-    expect_output(
-        object = headtail(matrix[seq_len(2L), seq_len(2L)]),
+    expect_match(
+        object = headtail(mat[seq_len(2L), seq_len(2L)])[[2L]],
         regexp = "gene1"
     )
 })
