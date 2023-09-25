c50184260d82c7f70d07d15ce6e9e03bad4ee572
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-25 13:03:54 -0400

    Improve documentation

--- R/strExtractAll.R
+++ R/strExtractAll.R
@@ -1,17 +1,23 @@
-## Extract all matches
-## shopping_list <- c("apples x4", "bag of flour", "bag of sugar", "milk x2")
-## stringr::str_extract_all(shopping_list, "[a-z]+")
-##
-## FIXME Need to fill with NA on match failure.
-
-
-
 #' Extract all pattern occurances
 #'
 #' Uses [gregexpr()] and [regmatches()] internally.
 #'
 #' @export
 #' @note Updated 2023-09-25.
+#'
+#' @param x `character`.
+#' Character vector. `NA` values are allowed.
+#'
+#' @param pattern `character(1)`.
+#' Regular expression pattern.
+#' Evalutes with [gregexpr()] internally.
+#'
+#' @param fixed `logical(1)`.
+#' If `TRUE`, `pattern` is a string to be matched as is.
+#' Otherwise, will match by regular expression.
+#'
+#' @return `list`.
+#' List of character vector extractions.
 #'
 #' @seealso
 #' - [stringi::stri_extract_all].
