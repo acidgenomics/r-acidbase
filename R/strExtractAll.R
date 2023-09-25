e751e27a638567679f3af58635f8af0c693dd138
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-25 12:58:47 -0400

    Work on improving string splitting consistency

--- R/strExtractAll.R
+++ R/strExtractAll.R
@@ -1,7 +1,37 @@
-## FIXME Add support for this.
-## FIXME Consider wrapping strMatchAll here.
-## FIXME Use gregexpr + regmatches here.
-
 ## Extract all matches
 ## shopping_list <- c("apples x4", "bag of flour", "bag of sugar", "milk x2")
 ## stringr::str_extract_all(shopping_list, "[a-z]+")
+##
+## FIXME Need to fill with NA on match failure.
+
+
+
+#' Extract all pattern occurances
+#'
+#' Uses [gregexpr()] and [regmatches()] internally.
+#'
+#' @export
+#' @note Updated 2023-09-25.
+#'
+#' @seealso
+#' - [stringi::stri_extract_all].
+#' - [stringr::str_extract_all].
+#' - https://stringr.tidyverse.org/articles/from-base.html
+#'
+#' @examples
+#' object <- strExtractAll(
+#'     x = c("apples x4", "bag of flour", "bag of sugar", "milk x2", NA),
+#'     pattern = "[a-z]+"
+#' )
+#' print(object)
+strExtractAll <- function(x, pattern, fixed = FALSE) {
+    assert(
+        is.character(x),
+        isString(pattern),
+        isFlag(fixed)
+    )
+    m <- gregexpr(pattern = pattern, text = x, fixed = fixed)
+    l <- regmatches(x = x, m = m)
+    l[is.na(x)] <- NA_character_
+    l
+}
