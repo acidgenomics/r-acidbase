3b620ab7ff32e4759db69f69c9d8803fcce80f85
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-25 13:41:06 -0400

    Add coverage

--- tests/testthat/test-strExtractAll.R
+++ tests/testthat/test-strExtractAll.R
@@ -1,61 +1,92 @@
-x <- c(
-    "aaa aa a",
-    "aa a",
-    "a",
-    "b",
-    "b bb",
-    "b bb bbb",
-    "b aa"
-)
-stringi::stri_extract_all_fixed(x, pattern = "a")
-stringi::stri_extract_all_regex(x, pattern = "a+")
-stringi::stri_extract_first_fixed(x, pattern = "a")
-stringi::stri_extract_first_regex(x, pattern = "a+")
-
-strExtractAll(
-    x = c("apples x4", "bag of flour", "bag of sugar", "milk x2", NA),
-    pattern = "[a-z]+"
-)
-strExtractAll(
-    c("apples x4", "bag of flour", "bag of sugar", "milk x2", NA),
-    "\\d"
-)
-
-stringr::str_extract_all(
-    c("apples x4", "bag of flour", "bag of sugar", "milk x2", NA),
-    "[a-z]+"
-)
-## [[1]]
-## [1] "apples" "x"
-##
-## [[2]]
-## [1] "bag"   "of"    "flour"
-##
-## [[3]]
-## [1] "bag"   "of"    "sugar"
-##
-## [[4]]
-## [1] "milk" "x"
-##
-## [[5]]
-## [1] NA
-##
-stringr::str_extract_all(
-    c("apples x4", "bag of flour", "bag of sugar", "milk x2", NA),
-    "\\d"
-)
-## [[1]]
-## [1] "4"
-##
-## [[2]]
-## character(0)
-##
-## [[3]]
-## character(0)
-##
-## [[4]]
-## [1] "2"
-##
-## [[5]]
-## [1] NA
-##
+test_that("Extract all matches", {
+    expect_identical(
+        object = strExtractAll(
+            x = c(
+                "aaa aa a",
+                "aa a",
+                "a",
+                "b",
+                "b bb",
+                "b bb bbb",
+                "b aa",
+                NA_character_
+            ),
+            pattern = "a",
+            fixed = TRUE
+        ),
+        expected = list(
+            rep("a", 6L),
+            rep("a", 3L),
+            "a",
+            character(),
+            character(),
+            character(),
+            rep("a", 2L),
+            NA_character_
+        )
+    )
+    expect_identical(
+        object = strExtractAll(
+            x = c(
+                "aaa aa a",
+                "aa a",
+                "a",
+                "b",
+                "b bb",
+                "b bb bbb",
+                "b aa",
+                NA_character_
+            ),
+            pattern = "a+",
+            fixed = FALSE
+        ),
+        expected = list(
+            c("aaa", "aa", "a"),
+            c("aa", "a"),
+            "a",
+            character(),
+            character(),
+            character(),
+            "aa",
+            NA_character_
+        )
+    )
+    expect_identical(
+        object = strExtractAll(
+            x = c(
+                "apples x4",
+                "bag of flour",
+                "bag of sugar",
+                "milk x2",
+                NA_character_
+            ),
+            pattern = "[a-z]+"
+        ),
+        expected = list(
+            c("apples", "x"),
+            c("bag", "of", "flour"),
+            c("bag", "of", "sugar"),
+            c("milk", "x"),
+            NA_character_
+        )
+    )
+    expect_identical(
+        object = strExtractAll(
+            x = c(
+                "apples x4",
+                "bag of flour",
+                "bag of sugar",
+                "milk x20",
+                NA_character_
+            ),
+            pattern = "\\d+"
+        ),
+        expected = list(
+            "4",
+            character(),
+            character(),
+            "20",
+            NA_character_
+        )
+    )
+})
