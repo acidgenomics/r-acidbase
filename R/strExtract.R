e6d080e81b14fba52513d5f97cea30d91e83dd86
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-25 12:47:04 -0400

    Work on adding these base R variants

--- R/strExtract.R
+++ R/strExtract.R
@@ -1,3 +1,6 @@
 ## FIXME Add support for this.
 ## FIXME Consider just wrapping strMatch here.
 ## FIXME Use regexpr + regmatches here.
+
+## shopping_list <- c("apples x4", "bag of flour", "bag of sugar", "milk x2")
+## stringr::str_extract(shopping_list, "\\d")
