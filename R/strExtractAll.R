a0fe2961bb3145f1c50b6c12b1f2d591b865eaba
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-25 12:47:04 -0400

    Work on adding these base R variants

--- R/strExtractAll.R
+++ R/strExtractAll.R
@@ -1,3 +1,7 @@
 ## FIXME Add support for this.
 ## FIXME Consider wrapping strMatchAll here.
 ## FIXME Use gregexpr + regmatches here.
+
+## Extract all matches
+## shopping_list <- c("apples x4", "bag of flour", "bag of sugar", "milk x2")
+## stringr::str_extract_all(shopping_list, "[a-z]+")
