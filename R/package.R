8d98b12c3262ad78ae39497dde28f174e9a31a8e
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-26 15:12:19 -0400

    Add a dupes utility function

--- R/package.R
+++ R/package.R
@@ -32,8 +32,8 @@ NULL
 #' @importFrom goalie allAreAtomic allAreDirs allHaveAccess allAreMatchingFixed
 #' allAreMatchingRegex assert bapply compressExtPattern extPattern hasDims
 #' hasLength hasNames hasNoDuplicates isADir isAFile isAGitRepo isASystemCommand
-#' isAURL isAny isCharacter isFlag isInRange isInstalled isInt isPositive
-#' isScalar isString isSubset isSystemCommand requireNamespaces
+#' isAURL isAny isCharacter isDuplicate isFlag isInRange isInstalled isInt
+#' isPositive isScalar isString isSubset isSystemCommand requireNamespaces
 #' @importFrom methods .hasSlot getClassDef is isGeneric selectMethod
 #' setClassUnion setMethod setOldClass signature slot slotNames
 NULL
