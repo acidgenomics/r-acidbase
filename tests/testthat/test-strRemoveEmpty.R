41e4ffcaf96714d61079befca852d94983aaf17e
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-28 10:09:43 -0400

    Add option to ignore spaces

--- tests/testthat/test-strRemoveEmpty.R
+++ tests/testthat/test-strRemoveEmpty.R
@@ -1,11 +1,11 @@
 test_that("strRemoveEmpty", {
     x <- c("", "a", "b", " ", NA_character_)
     expect_identical(
-        object = strRemoveEmpty(x, naOk = TRUE),
+        object = strRemoveEmpty(x, naOk = TRUE, spacesOk = TRUE),
         expected = c("a", "b", " ", NA_character_)
     )
     expect_identical(
-        object = strRemoveEmpty(x, naOk = FALSE),
-        expected = c("a", "b", " ")
+        object = strRemoveEmpty(x, naOk = FALSE, spacesOk = FALSE),
+        expected = c("a", "b")
     )
 })
