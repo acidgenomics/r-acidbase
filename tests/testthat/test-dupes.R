67b69e50a303f965d74128e5d337efd9bf740941
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-15 13:15:21 -0500

    Add notDupes coverage

--- tests/testthat/test-dupes.R
+++ tests/testthat/test-dupes.R
@@ -4,15 +4,31 @@ test_that("character", {
         expected = c("ccc", "bbb")
     )
     expect_identical(
+        object = notDupes(c("ccc", "aaa", "bbb", "ccc", "bbb")),
+        expected = "aaa"
+    )
+    expect_identical(
         object = dupes(c(NA_character_, NA_character_, "aaa", "aaa")),
         expected = c(NA_character_, "aaa")
+    )
+    expect_identical(
+        object = notDupes(c(NA_character_, NA_character_, "aaa", "aaa")),
+        expected = character()
     )
     expect_identical(
         object = dupes(c("a", "b", "c")),
         expected = character()
     )
     expect_identical(
+        object = notDupes(c("a", "b", "c")),
+        expected = c("a", "b", "c")
+    )
+    expect_identical(
         object = dupes(character()),
+        expected = character()
+    )
+    expect_identical(
+        object = notDupes(character()),
         expected = character()
     )
 })
@@ -23,19 +39,27 @@ test_that("numeric", {
         expected = integer()
     )
     expect_identical(
+        object = notDupes(seq(from = 1L, to = 3L)),
+        expected = seq(from = 1L, to = 3L)
+    )
+    expect_identical(
         object = dupes(c(3L, 2L, 3L, 2L, 1L)),
         expected = c(3L, 2L)
+    )
+    expect_identical(
+        object = notDupes(c(3L, 2L, 3L, 2L, 1L)),
+        expected = 1L
     )
 })
 
 test_that("logical", {
     expect_false(dupes(c(TRUE, FALSE, FALSE)))
-    expect_identical(
-        object = dupes(c(NA, NA)),
-        expected = NA
-    )
+    expect_true(notDupes(c(TRUE, FALSE, FALSE)))
+    expect_identical(dupes(c(NA, NA)), NA)
+    expect_identical(notDupes(c(NA, NA)), logical())
 })
 
 test_that("NULL", {
     expect_null(dupes(NULL))
+    expect_null(notDupes(NULL))
 })
